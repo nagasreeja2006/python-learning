c8dcadf8b99d9411793d2ae026416cbe310fcb80
nagasreeja2006 <pulivarthisreeja2006@gmail.com>
2026-08-19 00:14:34 +0530

    Add basic variables and student input program
    Implement basic variable declarations and input handling for student details.

---
+++ day1.py
@@ -0,0 +1,25 @@
+ #day1 of python learning (basic variables and input)
+"""
+str="sri"
+integer=1
+double=10.00
+float=10.0
+print(str)
+print(integer)
+print(double)
+print(float)
+
+#datatypes
+print(type(str))
+print(type(integer))
+print(type(double))
+print(type(float))
+"""
+
+#basic program to print student details using input
+student_name=input("enter student name: ")
+student_age=int(input("enter student age: "))
+branch=input("enter branch:")
+print("student name:",student_name)
+print("student age:",student_age)
+print("branch:",branch)
